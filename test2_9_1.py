c199b281ef69ee40ef2788531b2f37f4fda2da65
Chao Yang <250310130@qq.com>
2017-07-21 19:08:39 +0800

    j

--- test2_9_1.py
+++ test2_9_1.py
@@ -13,7 +13,7 @@ def fanta_recursive(n, from_p,tmp_p,to_p ):
 
 if __name__ == '__main__' :
 	start = time.time()
-	fanta_recursive(3,'X','Y','Z')
+	fanta_recursive(15,'X','Y','Z')
 	end = time.time()
 	print (start,end,end - start)
 	
--- test2_9_2.py
+++ test2_9_2.py
@@ -3,28 +3,42 @@ import math
 
 def fanta_iteration(n, from_p,tmp_p,to_p,lst1,lst2,lst3 ):
 	''' iteration   fanta '''
-	
-	clockwise ={
-	for i in ( range(1 ,2**n ):
+	clockwise ={ 'from_p' : 'tmp_p' , 'tmp_p' : 'to_p', 'to_p' : 'from_p'}
+	counterclockwise = { 'from_p' : 'to_p', 'to_p' : 'tmp_p' , 'tmp_p' : 'from_p' }
+	clockwise_lst = { 'lst1' : 'lst2' , 'lst2' : 'lst3' , 'lst3' : 'lst1' }
+	counterclockwise_lst = { 'lst1' : 'lst3' , 'lst3' : 'lst2' , 'lst2' : 'lst1' }
+	for i in ( range(1 ,2**n ) ):
 		m = i ^ (i-1)
-		m =  math.log(m)/math.log(2) +1
-		if lst1[-1] == m :
+		m = int( math.log(m)/math.log(2) +1)
+		flag1 = 0
+		flag2 = 0
+		if len(lst1)>0 and lst1[-1] == m :
+			print('a')
 			from_plate = from_p
 			from_list = lst1
-		elif lst2[-1] == m :
+			flag1 = 'from_p'
+			flag2 = 'lst1'
+		elif len(lst2)>0 and lst2[-1] == m :
+			print('b')
 			from_plate = tmp_p
 			from_list = lst2
+			flag1 = 'tmp_p'
+			flag2 = 'lst2'
 		else :
+			print('c')
 			from_plate = to_p
 			from_list = lst3
+			flag1 = 'to_p'
+			flag2 = 'lst3'
 		if (n+m)%2 :
-			to_plate =  
-			to_list =
+			to_plate =  eval(clockwise[flag1])
+			to_list =   eval(clockwise_lst[flag2] )
 		else :
-			to_plate =
-			to_list =
+			to_plate = eval(counterclockwise[flag1])
+			to_list =  eval(counterclockwise_lst[flag2])
 		print ("plate %d : %s --> %s" %(m,from_plate,to_plate) )
-		plate = from_plate.pop()
+		
+		plate = from_list.pop()
 		to_list.append(plate)
 	
 				
@@ -32,8 +46,8 @@ def fanta_iteration(n, from_p,tmp_p,to_p,lst1,lst2,lst3 ):
 
 if __name__ == '__main__' :
 	start = time.time()
-	N = 2
-	fanta_recursive( N,'X','Y','Z',[1:N+1],[],[] )
+	N = 15
+	fanta_iteration( N,'X','Y','Z',range(N,0,-1),[],[] )
 	end = time.time()
 	print (start,end,end - start)
 	
